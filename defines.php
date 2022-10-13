b1dbaf7ba06123b7761141c5a2fc1595530d2d12
Juwan <jhollingsworth5@student.clayton.edu>
2022-10-13 18:05:26 -0400

    Implement get user info

--- defines.php
+++
@@ -1,4 +0,0 @@
-<?php
-    define('INSTAGRAM_APP_ID','657018599126328');
-    define('INSTAGRAM_APP_SECRET','2b0731f449baf0be700efcee0e251212');
-    define('INSTAGRAM_APP_REDIRECT_URI','https://test-app-display.herokuapp.com/');
