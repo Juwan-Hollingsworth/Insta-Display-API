e6056bc55dc54fee7c0ec1e300d997ba4b5ab757
Juwan <jhollingsworth5@student.clayton.edu>
2022-10-13 18:05:46 -0400

    pt2

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 
         //check for code in the URL if one exist pass it along if not set to empty
         $params = array(
-            'get_code' => isset($_GET['code'])? $_GET['code'] : ''
+            'get_code' => isset( $_GET['code'] ) ? $_GET['code'] : '',
+            'access_token' => $accessToken
         );
 
 
@@ -18,6 +19,21 @@
     <!-- if user has token - display content + show token -->
     <?php if($ig->hasUserAccessToken): ?>
         <h4>IG Info</h4>
+        
+        <?php $user = $ig->getUser(); ?>
+        
+        <hr />
+	<?php $user = $ig->getUser(); ?>
+	<pre>
+		<?php print_r( $user ); ?>
+	</pre>
+	<h1>Username: <?php echo $user['username']; ?></h1>
+	<h2>IG ID: <?php echo $user['id']; ?></h2>
+	<h3>Media Count: <?php echo $user['media_count']; ?></h3>
+	<h4>Account Type: <?php echo $user['account_type']; ?></h4>
+	<hr />
+
+
         <h6>Access Token </h6>
         <?php echo $ig->getUserAccessToken(); ?>
         <h6>Expires In:</h6>
