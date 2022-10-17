af6f88c69050f6795c0ad8458f4a8e1feb0d45bd
Juwan <jhollingsworth5@student.clayton.edu>
2022-10-16 21:37:17 -0400

    Revert back to stable state - Get user info
    This reverts commit a7ea92cb00b3ba883cf7768ed2e0667257b046e9.

--- ig_basic_display_api.php
+++ ig_basic_display_api.php
@@ -1,23 +1,32 @@
 <?php 
     require_once('defines.php');
 
+
+    //Class that calls to IG Basic Display API
     Class ig_basic_display_api {
+
+        //declare variables
         private $_appId = INSTAGRAM_APP_ID;
 		private $_appSecret = INSTAGRAM_APP_SECRET;
 		private $_redirectUrl = INSTAGRAM_APP_REDIRECT_URI;
         private $_getCode = '';
         private $_apiBaseUrl = 'https://api.instagram.com/';
+        private $_graphBaseUrl ='https://graph.instagram.com/';
         private $_userAccessToken = '';
-
+        private $_userAccessTokenExpires = '';
+       
         public $authorizationUrl = '';
         public $hasUserAccessToken = false;
 
+
+        //call f(x) when Class is instantiated.
         function __construct( $params ){
             //save instagram code
+            //get_code = IG URL Identifier 
             $this->_getCode = $params['get_code'];
             //get a access token 
             $this->_setUserInstagramAccessToken ( $params );
-            //get authorization url
+            //if no access token in URL generate authorization url. 
             $this->_setAuthorizationUrl();
         }
 
@@ -26,6 +35,15 @@
 
         }
 
+        //return token in secs
+        public function getUserAccessTokenExpires(){
+            return $this->_userAccessTokenExpires;
+
+        }
+        //get auth URL
+        // send app_id, redirect URL, scope & response type as varaibles
+        // scope =  access  permission 
+        //send users to instagram to auth w/ app
         private function _setAuthorizationUrl(){
             $getVars = array( 
 				'app_id' => $this->_appId,
@@ -39,13 +57,30 @@
         }
 
         private function _setUserInstagramAccessToken( $params ){
-            // if get code is available> access token
-            if ($params['get_code']) {
-                $userAccessTokenResponse = $this->_getUserAccessToken();
-                    $this->_userAccessToken = $userAccessTokenResponse['access_token'];
-                    $this->hasUserAccessToken = true;
+            // check if we are passing an access token
+            if ($params['access_token']){
+                $this->_userAccessToken = $params['access_token'];
+                $this->hasUserAccessToken = true;
+            }
+            // if get code exist > get access token
+            // short lived access token
+            elseif ( $params['get_code'] ) { // try to get a access token
+				$userAccessTokenResponse = $this->_getUserAccessToken();
+				$this->_userAccessToken = $userAccessTokenResponse['access_token'];
+				$this->hasUserAccessToken = true;
+
+
+                //get long lived access token
+				$longLivedAccessTokenResponse = $this->_getLongLivedUserAccessToken();
+				$this->_userAccessToken = $longLivedAccessTokenResponse['access_token'];
+                //get time token expires (secs)
+				$this->_userAccessTokenExpires = (int)$longLivedAccessTokenResponse['expires_in'];
+            }
+
+
         }
-    }
+
+        // build access token request params
         private function _getUserAccessToken() {
             $params = array (
                 'endpoint_url' => $this->_apiBaseUrl . 'oauth/access_token',
@@ -58,31 +93,81 @@
 					'code' => $this->_getCode
                 )
             );
+
+            //make an API call passing in params
+            // return response payload
             $response = $this->_makeApiCall( $params );
 			return $response;
         }
 
+
+        public function getUser(){
+            $params = array(
+				'endpoint_url' => $this->_graphBaseUrl . 'me',
+				'type' => 'GET',
+				'url_params' => array(
+					'fields' => 'id,username,media_count,account_type',
+				)
+                );
+
+                $response = $this->_makeApiCall( $params );
+			return $response;
+
+        }
+
+
+
+
+        //requires different endpoint URL
+        //graph base URL
+       private function _getLongLivedUserAccessToken(){
+        $params = array(
+            'endpoint_url' => $this->_graphBaseUrl . 'access_token',
+            'type' => 'GET',
+            'url_params' => array(
+                'client_secret' => $this->_appSecret,
+                'grant_type' => 'ig_exchange_token',
+            )
+        );
+        $response = $this->_makeApiCall( $params );
+        return $response;
+
+       }
+
+        // make API call via Curl
         private function _makeApiCall( $params){
+            //intitialize curl
             $ch = curl_init();
+            //set endpoint
             $endpoint = $params['endpoint_url'];
 
-            if ( 'POST' == $params['type'] ) { // post request
+            //check what type of call
+            // POST req settings different from GET
+            if ( 'POST' == $params['type'] ) { 
+                //POST req. build query based on URL params
 				curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $params['url_params'] ) );
 				curl_setopt( $ch, CURLOPT_POST, 1 );
-			}
+			} elseif ('GET' == $params['type']){
+                //get request
+                $params['url_params']['access_token'] = $this->_userAccessToken;
+
+                //add params to endpoint
+                $endpoint .= '?' . http_build_query($params['url_params']);
+            }
 
             //general curl options
             curl_setopt($ch, CURLOPT_URL, $endpoint);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
+            //get response from instagram via curl
             $response = curl_exec( $ch);
-            
+            //close curl call
             curl_close( $ch);
-
+            // store response in php arr
             $responseArray = json_decode($response, true);
-
+            //if err show on page & close
+            //!if no err return response
             if (isset ($responseArray['error_type'])){
                 var_dump( $responseArray);
                 die();
--- index.php
+++ index.php
@@ -3,7 +3,8 @@
         require_once('ig_basic_display_api.php');
 
         $params = array(
-            'get_code' => isset($_GET['code'])? $_GET['code'] : ''
+            'get_code' => isset($_GET['code'])? $_GET['code'] : '',
+            'access_token' => $accessToken
         );
 
 
@@ -16,6 +17,21 @@
     <hr/>
     <?php if($ig->hasUserAccessToken): ?>
         <h4>IG Info</h4>
+        
+        <?php $user = $ig->getUser(); ?>
+        
+        <pre>
+            <?php print_r($user); ?>
+        </pre>
+
+    <h1>Username: <?php echo $user['username']; ?><h1>
+    <h1>IG ID: <?php echo $user['id']; ?><h1>
+    <h1>Media Count: <?php echo $user['media_count']; ?><h1>
+    <h1>Follower Count: <?php echo $user['followers_count']; ?><h1>
+    <h1>Account Type: <?php echo $user['account_type']; ?><h1>
+
+
+        <h6>Access Token </h6>
         <?php echo $ig->getUserAccessToken(); ?>
      <?php else: ?>
         <a href="<?php echo $ig->authorizationUrl; ?>"> Authorize Here
