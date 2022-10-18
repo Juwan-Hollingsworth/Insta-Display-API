22208c69c217e44e473fd4a77fae097a769d7934
Juwan <jhollingsworth5@student.clayton.edu>
2022-10-17 22:09:00 -0400

    add defines file to git ignore

--- defines.php
+++ defines.php
@@ -1,5 +1,5 @@
 <?php
-    define('INSTAGRAM_APP_ID','768638091102065');
-    define('INSTAGRAM_APP_SECRET','fed6b1b929bec89f8df2e57f6125429d');
-    define('INSTAGRAM_APP_REDIRECT_URI','https://test-app-display.herokuapp.com/');
-    define('INSTAGRAM_APP_SETTING','fed6b1b929bec89f8df2e57f6125429d');
+    define('INSTAGRAM_APP_ID','YOUR_FB_APP_ID');
+    define('INSTAGRAM_APP_SECRET','YOUR_FB_APP_SECRET');
+    define('INSTAGRAM_APP_REDIRECT_URI','HTTPS://YOURSITEREDIRECT.COM/');
+    
